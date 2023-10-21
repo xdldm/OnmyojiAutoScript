25342f40d1e96c6dd4cf6ce509896c46b2cdb768
xdldm <haohaonan123123@outlook.com>
2023-10-21 17:44:44 +0800

    翻译

--- tasks/CommonActivityShikigami/config.py
+++ tasks/CommonActivityShikigami/config.py
@@ -1,7 +1,6 @@
 # This Python file uses the following encoding: utf-8
 # @author runhey
 # github https://github.com/runhey
-from datetime import time, timedelta
 from pydantic import BaseModel, Field
 
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
@@ -9,14 +8,8 @@ from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase, TimeDelta
 from tasks.Component.BaseActivity.config_activity import GeneralClimb
 
-class CommonShikigamiConfig(BaseModel):
-    limit_time: timedelta = Field(default=timedelta(hours=0, minutes=30, seconds=0), description='limit_time_help')
-    limit_count: int = Field(default=10, description='limit_count_help')
-
-
 class CommonActivityShikigami(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
-    # shikigami: ShikigamiConfig = Field(default_factory=ShikigamiConfig)
     general_climb: GeneralClimb = Field(default_factory=GeneralClimb)
     general_battle: GeneralBattleConfig = Field(default_factory=GeneralBattleConfig)
 
